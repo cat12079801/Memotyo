94c595b1f8379303825b16f164fcac0de5bccef5
cat12079801 <cat12079801@gmail.com>
2015-03-01 11:53:43 +0900

    twitter id を変数にした

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ tw.stream('user', {'replies': 'all'}, function(stream) {
   stream.on('data', function (tw_data) {
     if(tw_data.user === undefined || tw_data.entities.user_mentions[0] === undefined){
       console.log(tw_data);
-    }else if(tw_data.entities.user_mentions[0].id == 3043673340 && tw_data.entities.user_mentions[1] === undefined){
+    }else if(tw_data.entities.user_mentions[0].id == conf.my_twitter_id && tw_data.entities.user_mentions[1] === undefined){
       //console.log(tw_data.text);
       //var date = new Date();
       //tw.updateStatus(date.getHours() + "時" + date.getMinutes() + "分" + date.getSeconds() + "秒をお知らせします", {}, function(err, tw_data){});
@@ -81,7 +81,7 @@ tw.stream('user', {'replies': 'all'}, function(stream) {
         if(user !== null){
           existing_user(user, tw_data);
         }else{
-          tw.getFollowersIds(3043673340, function(err, follower_ids){
+          tw.getFollowersIds(conf.my_twitter_id, function(err, follower_ids){
             if(follower_ids.indexOf(tw_data.user.id) != -1){
               new_user(user, tw_data);
             }else{
